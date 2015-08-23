95e29f1edf04385072016976a501cb2f0a3d4d19
Wojciech A. Koszek <wojciech@koszek.com>
2015-08-23 02:19:42 -0700

    Fix the release script.

--- rel.sh
+++ rel.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-#
+# Some stuff is based on:
 # https://gist.github.com/audreyr/5990987
-#
+
+if [ "x$1" = "x-t" ]; then
+	MODE="test"
+else
+	MODE=""
+fi
+
+echo "# Will build and register to PyPi: $MODE"
 
 (
 	cd godaddycli/
-	python setup.py sdist
-	python setup.py test
-	python setup.py upload
+	python setup.py register -r pypi${MODE}
+	python setup.py sdist upload -r pypi${MODE}
 )
-
